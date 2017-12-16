98c7dd36548dee318e6bc6733d69bbef06ab5a45
avhimkov <avhimkov@mail.ru>
2017-12-16 15:03:05 +0500

    add templates to dot folder for auth.go and delete example

--- auth.go
+++ auth.go
@@ -10,7 +10,7 @@ import (
 )
 
 type (
-	ExampleStruct struct {
+	AuthStruct struct {
 		ID          uint64    `form:"id"  json:"id"  bson:"_id"`
 		Name        string    `form:"name" json:"name" bson:"name" valid:"required"`
 		Description string    `form:"description" json:"description" bson:"description"`
@@ -18,7 +18,7 @@ type (
 		Updated     time.Time `form:"-" json:"updated" bson:"updated"`
 		Deleted     bool      `form:"-" json:"deleted" bson:"deleted"`
 	}
-	ExampleModule struct {
+	AuthModule struct {
 		summer.Module
 	}
 
@@ -27,29 +27,30 @@ type (
 
 var (
 
-	example = panel.AddModule(
+	auth = panel.AddModule(
 		&summer.ModuleSettings{
-			Name:           "example",
-			Title:          "My Example",
+			Name:           "auth",
+			CollectionName: "Auth",
+			Title:          "Auth in system",
 			MenuOrder:      0,
-			MenuTitle:      "My Example",
+			MenuTitle:      "Auth in system",
 			Rights:         summer.Rights{Groups: []string{"all"}},
 
 			Menu:           panel.MainMenu,
 		},
-		&ExampleModule{},
+		&UsersModule{},
 	)
 
 )
 
 
 // Add new record
-func (m *ExampleModule) Add(c *gin.Context) {
-	var result TasksStruct
+func (m *AuthModule) Add(c *gin.Context) {
+	var result AuthStruct
 	if !summer.PostBind(c, &result) {
 		return
 	}
-	result.ID = panel.AI.Next("Example")
+	result.ID = panel.AI.Next("auth")
 	result.Created = time.Now()
 	result.Updated = time.Now()
 	result.Name = sanitize.HTML(result.Name)
@@ -63,10 +64,10 @@ func (m *ExampleModule) Add(c *gin.Context) {
 }
 
 // Edit record
-func (m *ExampleModule) Edit(c *gin.Context) {
+func (m *AuthModule) Edit(c *gin.Context) {
 	id := types.Uint64(c.PostForm("id"))
-	var result ExampleStruct
-	var newValue ExampleStruct
+	var result UsersStruct
+	var newValue UsersStruct
 	if !summer.PostBind(c, &newValue) {
 		return
 	}
@@ -83,9 +84,9 @@ func (m *ExampleModule) Edit(c *gin.Context) {
 }
 
 // Get record from DB
-func (m *ExampleModule) Get(c *gin.Context) {
+func (m *AuthModule) Get(c *gin.Context) {
 	id := types.Uint64(c.PostForm("id"))
-	result := ExampleStruct{}
+	result := AuthStruct{}
 	if err := m.Collection.FindId(id).One(&result); err != nil {
 		c.String(404, "Not found")
 	}
@@ -93,23 +94,15 @@ func (m *ExampleModule) Get(c *gin.Context) {
 }
 
 // GetAll records
-func (m *ExampleModule) GetAll(c *gin.Context) {
+func (m *AuthModule) GetAll(c *gin.Context) {
 	filter := struct {
-		Sort    string `form:"sort"  json:"sort"`
 		Search  string `form:"search"  json:"search"`
 		Page    int    `form:"page"  json:"page"`
 		Deleted bool   `form:"deleted"  json:"deleted"`
 	}{}
 	summer.PostBind(c, &filter)
-	results := []ExampleStruct{}
+	results := []AuthStruct{}
 	request := obj{"deleted": filter.Deleted }
-
-	sort := "-_id"
-
-	// sort engine
-	if len(filter.Sort) > 0 {
-		sort = filter.Sort
-	}
 
 	// search engine
 	if len(filter.Search) > 0 {
@@ -130,7 +123,7 @@ func (m *ExampleModule) GetAll(c *gin.Context) {
 	}
 
 	// request to DB
-	if err := m.Collection.Find(request).Sort(sort).Limit(limit).Skip(skip).All(&results); err != nil {
+	if err := m.Collection.Find(request).Sort("-_id").Limit(limit).Skip(skip).All(&results); err != nil {
 		c.String(404, "Not found")
 		return
 	}
@@ -139,7 +132,7 @@ func (m *ExampleModule) GetAll(c *gin.Context) {
 }
 
 // Action - remove/restore record
-func (m *ExampleModule) Action(c *gin.Context) {
+func (m *AuthModule) Action(c *gin.Context) {
 	id := types.Uint64(c.PostForm("id"))
 
 	if err := m.Collection.UpdateId(id, obj{"$set": obj{"deleted": c.PostForm("action") == "remove" }}); err != nil {
@@ -148,3 +141,5 @@ func (m *ExampleModule) Action(c *gin.Context) {
 	}
 	c.JSON(200, obj{"data": obj{"id": id}})
 }
+
+
