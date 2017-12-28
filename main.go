aa64ee5719c465a9763c381998cd2b2a81150438
Dmitry <avhimkov@mail.ru>
2017-12-28 21:36:47 +0500

    reload project

--- main.go
+++ main.go
@@ -1,36 +1,7 @@
 package main
 
-import (
-	
-	"fmt"
-	"gopkg.in/night-codes/summer.v1"
-)
+import ("fmt")
 
-
-type (
-	obj map[string]interface{}
-	arr []interface{}
-)
-
-var (
-	panel = summer.Create(summer.Settings{
-		Title:       "My project",
-		Port:        8080,
-		DefaultPage: "hello",
-		Path:        "", // application path
-		DBName:      "project",
-		Views:       "templates/main",
-		ViewsDoT:    "templates/dot", // doT.js templates
-		FirstStart: func() { // some DB migrations etc.
-			fmt.Println("Application is running for the first time!")
-		},
-		Debug: summer.Env("production", "") == "", // set env. var "production" for Debug:false
-		JS:    []string{},                         // add custom JS files to template
-		CSS:   []string{},                         // add custom CSS files to template
-	})
-)
-
-func main() {
-	fmt.Println("Application started at http://localhost:8080/")
-	summer.Wait()
+func main()  {
+	fmt.Println("Help")
 }
