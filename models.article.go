0540f30c8647e83904b03101a00341967024810c
Dmitry <avhimkov@mail.ru>
2018-01-02 22:17:52 +0500

    refactoring models.article

--- models.article.go
+++ models.article.go
@@ -1,5 +1,7 @@
 package main
 
+import "errors"
+
 type article struct {
 	ID      int    `json:"id"`
 	Title   string `json:"title"`
@@ -17,4 +19,25 @@ var articleList = []article{
 // Return a list of all the articles
 func getAllArticles() []article {
 	return articleList
+}
+
+// Fetch an article based on the ID supplied
+func getArticleByID(id int) (*article, error) {
+	for _, a := range articleList {
+		if a.ID == id {
+			return &a, nil
+		}
+	}
+	return nil, errors.New("Article not found")
+}
+
+// Create a new article with the title and content provided
+func createNewArticle(title, content string) (*article, error) {
+	// Set the ID of a new article to one more than the number of articles
+	a := article{ID: len(articleList) + 1, Title: title, Content: content}
+
+	// Add the article to the list of articles
+	articleList = append(articleList, a)
+
+	return &a, nil
 }
