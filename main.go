da608c34a0d6980c11a39a541d855737364b40fb
Dmitry <avhimkov@mail.ru>
2018-01-02 22:20:46 +0500

    refactoring main.go

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
 var router *gin.Engine
 
 func main() {
+	// Set Gin to production mode
+	gin.SetMode(gin.ReleaseMode)
 
 	// Set the router as the default one provided by Gin
 	router = gin.Default()
@@ -20,26 +22,26 @@ func main() {
 	// Initialize the routes
 	initializeRoutes()
 
-	// Define the route for the index page and display the index.html template
-	// To start with, we'll use an inline route handler. Later on, we'll create
-	// standalone functions that will be used as route handlers.
-	router.GET("/", func(c *gin.Context) {
-
-		// Call the HTML method of the Context to render a template
-		c.HTML(
-			// Set the HTTP status to 200 (OK)
-			http.StatusOK,
-			// Use the index.html template
-			"index.html",
-			// Pass the data that the page uses (in this case, 'title')
-			gin.H{
-				"title": "Home Page",
-			},
-		)
-
-	})
-
 	// Start serving the application
 	router.Run()
+}
 
+// Render one of HTML, JSON or CSV based on the 'Accept' header of the request
+// If the header doesn't specify this, HTML is rendered, provided that
+// the template name is present
+func render(c *gin.Context, data gin.H, templateName string) {
+	loggedInInterface, _ := c.Get("is_logged_in")
+	data["is_logged_in"] = loggedInInterface.(bool)
+
+	switch c.Request.Header.Get("Accept") {
+	case "application/json":
+		// Respond with JSON
+		c.JSON(http.StatusOK, data["payload"])
+	case "application/xml":
+		// Respond with XML
+		c.XML(http.StatusOK, data["payload"])
+	default:
+		// Respond with HTML
+		c.HTML(http.StatusOK, templateName, data)
+	}
 }
